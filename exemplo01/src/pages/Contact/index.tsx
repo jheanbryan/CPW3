871d7805b73f716d9befd111ac78c729cce5e06d
Jhean Bryan <93744970+jheanbryan@users.noreply.github.com>
2024-10-03 21:59:00 -0400

    feat: initialized project, with React

---
+++ exemplo01/src/pages/Contact/index.tsx
@@ -0,0 +1,3 @@
+const Contact = () => <h1>Contact</h1>;
+
+export default Contact;
---
+++ exemplo01/src/pages/Home/index.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <div>
+            <a href="/contact">Contato</a>
+        </div>
+    )
+}
+
+export default Home;
