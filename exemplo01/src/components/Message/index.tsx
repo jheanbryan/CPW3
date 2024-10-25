9b76f9d258317bc588e08cc241016028cff594db
jheanbryan <jheanbeyan123@gmail.com>
2024-10-24 22:36:37 -0400

    feat: create error and sucess messages for user login

---
+++ exemplo01/src/components/Message/index.tsx
@@ -0,0 +1,25 @@
+import  styles  from "./styles.module.css";
+import { Severity } from "../../enums/Saverity";
+
+type Props = {
+    message: string;
+    severity: Severity;
+};
+
+const Message = ( {message, severity}: Props) => {
+    let severityStyle = styles.sucess;
+
+    if(severity == Severity.WARNING)
+        severityStyle = styles.warning;
+    else if (severity == Severity.ERROR)
+        severityStyle = styles.error;
+
+    return (
+        <div className={`${styles.container} ${severityStyle}`}>
+            <span>{message}</span>
+        </div>
+    )
+}
+
+
+export default Message;
