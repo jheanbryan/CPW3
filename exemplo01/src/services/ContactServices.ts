78635c1a56f0ae4b7cb0dceeeeba97a14cd1a735
Jhean Bryan <93744970+jheanbryan@users.noreply.github.com>
2024-11-07 18:48:58 -0400

    refact/updates in code

--- exemplo01/src/services/ContactServices.ts
+++ exemplo01/src/services/ContactServices.ts
@@ -1,4 +1,5 @@
 import {
+<<<<<<< HEAD
     collection,
     doc,
     getDoc,
@@ -60,3 +61,66 @@ import {
       return `${ownerEmail}|${contactEmail}`;
     }
   }
+=======
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  orderBy,
+  query,
+  setDoc,
+  where,
+} from "firebase/firestore";
+
+import { Contact, contactConverter } from "../models/Contact";
+import { store } from "../config/firebase";
+import { FirebaseContainer } from "../models/FirebaseContainer";
+
+export class ContactService {
+  async save(contact: Contact) {
+    const id = this._generateId(contact.ownerEmail, contact.email);
+    const ref = doc(
+      store,
+      FirebaseContainer.CONTACTS_COLLECTION_NAME,
+      id
+    ).withConverter(contactConverter);
+
+    await setDoc(ref, contact);
+  }
+
+  async findByOwnerEmailAndContactEmail(
+    ownerEmail: string,
+    contactEmail: string
+  ) {
+    const id = this._generateId(ownerEmail, contactEmail);
+    const ref = doc(
+      store,
+      FirebaseContainer.CONTACTS_COLLECTION_NAME,
+      id
+    ).withConverter(contactConverter);
+
+    const snapshot = await getDoc(ref);
+    return snapshot.data();
+  }
+
+  async findAllByOwner(ownerEmail: string) {
+    const ref = collection(store, FirebaseContainer.CONTACTS_COLLECTION_NAME);
+    const q = query(
+      ref,
+      where("ownerEmail", "==", ownerEmail),
+      orderBy("name")
+    );
+
+    const snapshot = await getDocs(q);
+
+    const contacts: Contact[] = [];
+    snapshot.forEach((doc) => contacts.push(new Contact(doc.data())));
+
+    return contacts;
+  }
+
+  private _generateId(ownerEmail: string, contactEmail: string) {
+    return `${ownerEmail}|${contactEmail}`;
+  }
+}
+>>>>>>> 7d0a37d (refact/updates in code)
