a6748b3050ec0f99be77ad85fb046173da09f26f
jheanbryan <jheanbeyan123@gmail.com>
2025-01-26 15:24:56 -0400

    feat: add search page

--- prova2/src/pages/Search/index.tsx
+++ prova2/src/pages/Search/index.tsx
@@ -1,19 +1,58 @@
-import { InputSearch, MainContainer } from "./styles";
-import  Header  from "../../components/Header/index";
+import React, { useState, useEffect } from 'react';
+import { DivLine, InputSearch, MainContainer } from "./styles";
+import Header from "../../components/Header/index";
+import { searchCrypto } from '../../services/cryptService';
 
 const Search = () => {
-    const 
+    const [cryptos, setCryptos] = useState<any | null>(null); 
+    const [searchTerm, setSearchTerm] = useState('');
+    const [debouncedTerm, setDebouncedTerm] = useState(searchTerm);
+
+    useEffect(() => {
+        if (debouncedTerm) {
+            const fetchCryptoData = async () => {
+                const result = await searchCrypto(debouncedTerm);
+                setCryptos(result);
+            };
+            fetchCryptoData();
+        }
+    }, [debouncedTerm]);
+
+    const handleInputChange = (e: any) => {
+        const value = e.target.value;
+        setSearchTerm(value);
+    };
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setDebouncedTerm(searchTerm);
+        }, 500);
+        return () => clearTimeout(timer);  
+    }, [searchTerm]);
 
     return (
         <>
-        <Header />
-        <MainContainer>
-            <InputSearch placeholder="Pesquise por uma criptomoeda" />
-        </MainContainer>
+            <Header />
+            <MainContainer>
+                <InputSearch
+                    placeholder="Pesquise por uma cripto"
+                    value={searchTerm}
+                    onChange={handleInputChange}
+                />
+                {cryptos && (
+                    <div>
+                        <h2>{cryptos.coin}</h2>
+                        <img src={cryptos.logo} alt={cryptos.coin} style={{ width: '50px', height: '50px' }} />
+                        <p>Ticker: {cryptos.ticker}</p>
+                        <p>Status: {cryptos.status}</p>
+                        <p>Minimum Transaction: {cryptos.minimum_transaction} {cryptos.minimum_transaction_coin}</p>
+                        <p>Minimum Fee: {cryptos.minimum_fee} {cryptos.minimum_fee_coin}</p>
+                        <p>Fee Percent: {cryptos.fee_percent}</p>
+                    </div>
+                )}
+            </MainContainer>
         </>
     );
-
-
 };
 
 export default Search;
--- prova2/src/pages/Search/styles.ts
+++ prova2/src/pages/Search/styles.ts
@@ -11,6 +11,14 @@ export const MainContainer = styled.div`
     margin: auto;
 `;
 
+export const DivLine = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 10px;
+`;
+
+
 export const InputSearch = styled.input`
     line-height: 24px;
     border: 1px solid #000;
@@ -19,6 +27,27 @@ export const InputSearch = styled.input`
     padding: 5px;
     max-width: 300px;
     width: 100%;
+    height: 2rem;
+`;
+
+export const ButtonSearch = styled.button`
+    padding: 5px 10px;
+    border-radius: 5px;
+    background-color: #fff;
+    color: #000;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 2rem;
+    cursor: pointer;
+    transition: color 0.3s, border-bottom 0.3s;
+    border: 2px solid transparent;
+
+    &:hover {
+    background-color: #50fa7b; /* Verde neon */
+    border-bottom: 2px solid #fff;
+  }
 `;
 
 export const CryptoCard = styled.div`
